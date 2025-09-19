c019a7092322b1f5612aabd51cc4dcb078bc66b0
hamzasiraj <36099837+hamzasiraj@users.noreply.github.com>
2025-09-20 00:46:46 +0500

    Update gold.js

--- api/gold.js
+++ api/gold.js
@@ -8,6 +8,6 @@ export default async function handler(req, res) {
 
     res.status(200).json(data); // Example: { "gold": { "usd": 1930.55 } }
   } catch (error) {
-    res.status(500).json({ error: "Failed to fetch gold price" });
+    res.status(500).json({ error: "Failed to fetch gold prrice" });
   }
 }
