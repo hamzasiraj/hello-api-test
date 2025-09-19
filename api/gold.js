7a08c180cf3bd590dc02ea9e07f3c62342463947
hamzasiraj <36099837+hamzasiraj@users.noreply.github.com>
2025-09-20 00:39:06 +0500

    Update gold.js

--- api/gold.js
+++ api/gold.js
@@ -2,7 +2,7 @@ export default async function handler(req, res) {
   try {
     const response = await fetch("https://www.goldapi.io/api/XAU/USD", {
       headers: {
-        "x-access-token": "fad784a8c01a5a78e84b8320f621e413",
+        "x-access-token": "633c0fb594c160e1b18ad59f7c83fed3633c0fb5",
         "Content-Type": "application/json"
       }
     });
